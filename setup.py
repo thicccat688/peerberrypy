f78eb6643565f2a5a264761c5e4ee72aacfbff61
thicccat688 <tomasperestrelo21@gmail.com>
2022-10-17 11:33:44 +0100

    Bump up version to match PyPi's version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='peerberrypy',
-    version='0.1.9',
+    version='0.1.10',
     license='MIT',
     author='Tomás Perestrelo',
     author_email='tomasperestrelo21@gmail.com',
