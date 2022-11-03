3db3b980376ef2c1d7f9c22f9cfd1da82a87d2a9
thicccat688 <tomasperestrelo21@gmail.com>
2022-11-03 11:26:36 +0000

    Bump up package version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='peerberrypy',
-    version='0.1.10',
+    version='0.1.12',
     license='MIT',
     author='Tomás Perestrelo',
     author_email='tomasperestrelo21@gmail.com',
